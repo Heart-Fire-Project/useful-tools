554663eebda1108f357c2c27e65abda17938081b
HeimNad <5278626@qq.com>
2023-12-26 23:50:06 +0800

    修正工具: 无法完整提取所有语言的问题

--- 数据包翻译提取/extract_translate_from_datapack.py
+++ 数据包翻译提取/extract_translate_from_datapack.py
@@ -1,3 +1,4 @@
+# -*- coding: UTF-8 -*-
 import os
 import json
 from jsonfinder import jsonfinder
@@ -5,6 +6,7 @@ from jsonfinder import jsonfinder
 directory = "data/"
 output_file = "data/lang/zh_cn.json"
 result = {}
+
 
 def process_json_object(json_object, result):
     if isinstance(json_object, dict):
@@ -15,16 +17,21 @@ def process_json_object(json_object, result):
         for value in json_object.values():
             if isinstance(value, (dict, list)):
                 process_json_object(value, result)
+    elif isinstance(json_object, list):
+        for item in json_object:
+            if isinstance(item, (dict, list)):
+                process_json_object(item, result)
+
 
 for root, dirs, files in os.walk(directory):
     for file in files:
         if file.endswith(".mcfunction"):
             filepath = os.path.join(root, file)
-            with open(filepath, "r") as f:
+            with open(filepath, "r", encoding="UTF-8") as f:
                 lines = f.readlines()
                 for line in lines:
                     for _, _, json_object in jsonfinder(line, json_only=True):
                         process_json_object(json_object, result)
 
-with open(output_file, "w") as f:
+with open(output_file, "w", encoding="UTF-8") as f:
     json.dump(result, f, ensure_ascii=False, indent=4)
